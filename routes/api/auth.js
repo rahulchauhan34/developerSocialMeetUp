46dd887823d565178073d1a258973ce59cfec0e0
RAHUL CHAUHAN <chauhanr@deakin.edu.au>
2021-04-13 01:14:32 +1000

    added landing and navbar

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,10 +8,11 @@ const config=require('config');
 const {check, validationResult}=require('express-validator');
 //add route  GET api/auth.  test route, public
 //
-router.get('/',auth,async (req,res)=>{
+router.get('/', (req,res)=>{
     try{
-        const user =  await User.findById(req.user.id).select('-password');
-        res.json(user);
+       // const user =  await User.findById(req.user.id).select('-password');
+        console.log(res);
+       // res.json(user);
     }
     catch(err){
         console.error(err.message);
